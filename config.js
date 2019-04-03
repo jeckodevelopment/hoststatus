7ac9f82ba056ed006fdb3007de2f7cdefdbcd771
Alessandro Menduni <alessandro.menduni@host.it>
2019-04-03 16:48:20 +0200

    Kickoff

---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "title": "Host Status",
+  "name": "host_status",
+  "baseUrl": "https://jeckodevelopment.github.io/hoststatus/",
+  "description": "",
+  "defaultLocale": "it",
+  "locales": [
+    {
+      "code": "it",
+      "iso": "it-IT",
+      "name": "Italiano",
+      "file": "it.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml",
+    "systems": [
+      'host-it',
+      'dns',
+      'hosting',
+      'server-dedicati'
+    ]
+  }
+}
---
+++ content/2019-04-03_attacco-ddos.md
@@ -0,0 +1,12 @@
+---
+title: Attacco DDos
+date: '2019-04-03T14:44:19.987Z'
+severity: partial-outage
+affectedsystems:
+  - host-it
+  - hosting
+resolved: false
+---
+L'attacco ha mandato tutto in fallimento
+
+<!--- language code: it -->
